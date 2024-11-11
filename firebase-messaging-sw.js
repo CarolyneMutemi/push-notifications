760f486e08fe91980a2853595a55901b059465f2
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-11-11 15:38:51 +0300

    On Background

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -18,13 +18,13 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Optional: Handle background message
-// messaging.onBackgroundMessage((payload) => {
-//     console.log("Received background message: ", payload);
-//     const notificationTitle = payload.notification.title;
-//     const notificationOptions = {
-//         body: payload.notification.body,
-//         icon: "./two.jpg",
-//     };
-//     self.registration.showNotification(notificationTitle, notificationOptions);
-// });
+messaging.onBackgroundMessage((payload) => {
+    console.log("Received background message: ", payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        // icon: "./two.jpg",
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
